9871440ddef25f434f5406945e4df50d66aadee0
Levi <levi@the-telling-detail.org>
2025-06-05 18:15:00 -0400

    a whole new approach

--- src/routes/intersections/[slug]/+page.ts
+++ src/routes/intersections/[slug]/+page.ts
@@ -1,12 +1,17 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch, params }) => {
-  const slug = params.slug.toUpperCase();
-  const poemTitle = `${slug[0]} + ${slug[1]}`;
+  const slug = params.slug;
+  const titleWords = slug.split('-');
+  const poemTitle = titleWords.map(w => capitalize(w)).join(' ');
 
-	const poem = await fetch(`/poems/${poemTitle}.txt`)
+	const poem = await fetch(`/poems/${slug}.txt`)
     .then((res) => res.text())
     .then((text) => text.split('\n'));
 
 	return { poemTitle, poem };
 };
+
+function capitalize(str: String) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
