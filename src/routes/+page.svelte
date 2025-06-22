c71f15007c393787b8570b686669302e84d647f6
Levi <levi@the-telling-detail.org>
2025-06-21 20:55:37 -0400

    non linked matrix numbers should drift along the y axis

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,15 +10,27 @@
   function routeForPoemAt(index: number) {
     return `/${data.metaTailMap[index].slug}`;
   }
+
+  function randomIntBetween(low: number, high: number): number {
+    return Math.floor(Math.random() * (high - low + 1)) + low;
+  }
 </script>
 
 <div class='matrix'>
   <span id='ellipsis'><a href="https://here-i-am.me/" target="_blank">&hellip;</a></span>
   {#each data.shortTailOfPi as digit, i}
     {#if metaAt(i) === undefined}
-      <span class='digit'>{digit}</span>
+      <span class='digit'>
+        <span class='y-drift' style="--duration: {randomIntBetween(3000, 8000)}ms; --distance: {randomIntBetween(15, 30)}%;">
+          {digit}
+        </span>
+      </span>
     {:else}
-      <span class='digit'><a href={routeForPoemAt(i)}>{digit}</a></span>
+      <span class='digit'>
+        <a class='x-drift' href={routeForPoemAt(i)}>
+          {digit}
+        </a>
+      </span>
     {/if}
   {/each}
 </div>
@@ -31,19 +43,33 @@
     margin: 1%;
   }
 
+  span#ellipsis {
+    font-family: 'Trebuchet MS';
+  }
+
   span {
     font-family: sans-serif;
     font-size: 30px;
     padding: 5%;
     color: dimgrey;
+    display: flex;
   }
 
   span a {
-    color: gold;
+    color: #BEEEFF;
     text-decoration: none;
   }
 
-  span#ellipsis {
-    font-family: 'Trebuchet MS';
+  .y-drift {
+    animation: yDrift var(--duration) linear infinite alternate;
+  }
+
+  @keyframes yDrift {
+    from {
+      transform: translateY(var(--distance));
+    }
+    to {
+      transform: translateY(calc(var(--distance) * -1));
+    }
   }
 </style>
