bda578468a2dc0c729da8a9ca3d7c1c0dd99b06f
Levi <levi@the-telling-detail.org>
2025-06-25 02:08:43 -0400

    update fonts

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -2,51 +2,32 @@
   import type { PageProps } from './$types';
 
   let { data }: PageProps = $props();
-  
-  let longestLineEl: HTMLElement | null = $state(null);
-
-  let fontSize = $state(60);
-
-  let lineLengths = $derived(data.poem.map(line => line.length));
-  let longestLineIndex = $derived(lineLengths.indexOf(Math.max(...lineLengths)));
-
-  $effect(() => {
-    if (longestLineEl && longestLineEl.scrollWidth > (window.outerWidth - 60)) {
-      smallerFontSize();
-    }
-  });
-  
-  function smallerFontSize() {
-    fontSize = fontSize - 1; 
-    console.log('font size', fontSize);
-  }
-
 </script>
 
 <h1 id='title'>{ data.title }</h1>
-<div id="poem" style:font-size={ `${fontSize}px`} >
+<div id="poem">
   {#each data.poem as line, i}
-    {#if i === longestLineIndex}
-      <span class='line longest' bind:this={ longestLineEl } >{line}</span>
-    {:else}
-      <span class='line'>{line}</span>
-    {/if}
+    <span class='line'>{line}</span>
   {/each}
 </div>
 
 <style>
   #title {
-    color: ghostwhite;
+    color: white;
     text-align: center;
-    font-family: "Lilita One", sans-serif;
+    font-family: "Montserrat", sans-serif;
+    font-weight: 400;
     font-size: 2em;
     margin: 15px 0 0;
     border-radius: 5px;
   }
 
   #poem {
-    color: whitesmoke;
-    font-family: "EB Garamond", serif;  
+    color: white;
+    font-family: "Newsreader", serif;
+    font-optical-sizing: auto;
+    font-weight: 200;
+    font-size: 40px;
     line-height: 1.4;
     margin: 0 5px 10px;
     padding: 10px 20px;
@@ -58,9 +39,5 @@
     display: block;
     width: fit-content;
     white-space: nowrap;
-  }
-
-  .line.longest {
-    color: #ff6347; /* tomato */
   }
 </style>
