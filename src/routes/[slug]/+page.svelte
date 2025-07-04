a8448ab403062c12ba1dabcf00c21c2ab95c329d
Levi <levi@the-telling-detail.org>
2025-07-04 19:35:46 -0400

    wham!

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,41 +1,70 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import type { PageProps } from './$types';
+  import fitty from 'fitty';
+
+  onMount(() => {
+    fitty('#text .line');
+    fitty('#title');
+  });
 
   let { data }: PageProps = $props();
 </script>
 
-<div id='page'>
-  <p id='title'>{ data.title }</p>
-  <div id="poem">
-    {#each data.poem as line, i}
-      <span class='line'>{line}</span>
-    {/each}
+<div id='surface'>
+  <div id='card'>
+    <div id='poem'>
+      <h1 id='title'>{ data.title }</h1>
+      <div id='text'>
+        {#each data.poem as line}
+          <span class='line'>{line}</span>
+        {/each}
+      </div>
+    </div>
   </div>
 </div>
 
 <style>
-  #page {
-    margin: 1px;
-    color: white;
+  #surface {
+    height: 100%;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
-
-  #title {
-    font-size: 0.7em;
-    margin: 5px 0;
-    width: fit-content;
-    white-space: nowrap;
-    text-align: center;
+  
+  #card {
+    width: 90%;
+    height: 95%;
+    border-radius: 5px;
+    padding: 5px;
+    background-color: #051021;
+    font-family: Arial, Helvetica, sans-serif;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
   }
 
   #poem {
-    font-size: 0.1em;
-    line-height: 1.3;
+    width: 80%;
+  }
+
+  #poem #title {
+    margin: 0 0 5%;
+  }
+  
+  #poem #text {
+    font-weight: 400;
     width: 100%;
   }
   
   .line {
     display: block;
     width: fit-content;
+    white-space: nowrap;
+    display: inline-block;
     white-space: nowrap;
   }
 </style>
