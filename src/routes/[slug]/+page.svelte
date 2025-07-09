21143e8b4e48c6dd7eaf7012572c900bfb0eb2f5
Levi <levi@the-telling-detail.org>
2025-07-09 02:10:29 -0400

    update style of poem vessel

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -5,26 +5,33 @@
 
   onMount(() => {
     fitty('#text .line');
-    fitty('#title');
+    // fitty('#title h5');
   });
 
   let { data }: PageProps = $props();
 </script>
 
-<div id='poem'>
-  <h1 id='title'>{ data.title }</h1>
-  <div id='text'>
-    {#each data.poem as line}
-      <span class='line'>{line}</span>
-    {/each}
+<div id='vessel'>
+  <div id='title'>
+    <h4>{ data.title }</h4>
+  </div>
+  <div id='poem'>
+    <div id='text'>
+      {#each data.poem as line}
+        <span class='line'>{line}</span>
+      {/each}
+    </div>
+  </div>
+  <div id='button'>
+    <button></button>
   </div>
 </div>
 
 <style>
-  #poem {
+  #vessel {
     height: 100%;
-    width: 80%;
-    margin: 0 auto;
+    width: 100%;
+    margin: 0;
     background-color: #051021;
     font-family: Arial, Helvetica, sans-serif;
     color: white;
@@ -33,21 +40,53 @@
     align-items: center;
     justify-content: center;
   }
-
-  #poem #title {
-    margin: 0 0 10%;
+  
+  #vessel #title {
+    flex-grow: 1;
+    width: 80%;
+    display: flex;
+    align-items: end;
   }
   
-  #poem #text {
-    font-weight: 400;
+  #title h4 {
+    border-bottom: 1px solid cornflowerblue;
+    margin: 0;
+  }
+  
+  #vessel #button {
+    flex-grow: 1;
     width: 100%;
+    display: flex;
+    align-items: start;
+    justify-content: center;
+  }
+  
+  #button button {
+    background-color: red;
+    border-radius: 50%;
+    width: 75px; 
+    height: 75px; 
+    border: 1px solid cornflowerblue; 
+    color: beige;
+    font-size: 16px; /* Adjust as needed */
+  }
+  
+  #vessel #poem {
+    height: 66%;
+    overflow-y: scroll;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  #vessel #text {
+    width: 80%;
+    font-weight: 400;
   }
   
   .line {
-    display: block;
-    width: fit-content;
     white-space: nowrap;
     display: inline-block;
-    white-space: nowrap;
   }
 </style>
