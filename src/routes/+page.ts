a5871b0a410aab7d36c2311bfa8804327c39f912
Levi <levi@the-telling-detail.org>
2025-07-30 16:00:16 -0400

    shortTailIndex shouldn't extend much beyond the digits that have linked poems

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -9,22 +9,21 @@ export const load: PageLoad = async ({ fetch, params }) => {
   const metaYml = await fetch('/poem-data.yml')
     .then((res) => res.text());
   const meta = yaml.load(metaYml) as PoemMetaData;
-  const nPoems = Object.keys(meta).length;
   
-  const shortTailOfPi = longTailOfPi.slice(0, nPoems * 100);
-  
-  const metaTailMap = new Array(shortTailOfPi.length);
+  const metaTailMap = new Array();
   const takenTailIndices: number[] = [];
   
   Object.keys(meta).forEach(slug => {
     const poemMetaData = meta[slug];
-    const tailIndex = shortTailOfPi.findIndex((d, i) => {
+    const tailIndex = longTailOfPi.findIndex((d, i) => {
       const digit = Number(d);
       return poemMetaData.sefirahId === digit && !takenTailIndices.includes(i);
     });
     metaTailMap[tailIndex] = { slug, ...poemMetaData };
     takenTailIndices.push(tailIndex);
   })
+
+  const shortTailOfPi = longTailOfPi.slice(0, metaTailMap.length + 40);
 
   return { metaTailMap, shortTailOfPi };
 }
