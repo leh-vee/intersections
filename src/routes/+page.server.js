decea59a76d34513df6cf26c87f9fc0c65e91754
Levi <levi@the-telling-detail.org>
2025-08-21 18:46:41 -0400

    first pass at map index

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,27 +1,5 @@
-// @ts-nocheck
 import { poemIndex } from '$lib/store.js';
 
-export async function load({ fetch }) {
-  const longTailOfPi = await fetch('/pi-long-tail.txt') 
-    .then((res) => res.text())
-    .then((text) => text.split(''));
-
+export async function load() {
   const keyedIndex = await poemIndex.get();
-  
-  const metaTailMap = new Array();
-  const takenTailIndices = [];
-
-  Object.keys(keyedIndex).forEach(slug => {
-    const sefirahId = keyedIndex[slug].sefirahId;
-    const tailIndex = longTailOfPi.findIndex((d, i) => {
-      return sefirahId === d && !takenTailIndices.includes(i);
-    });
-    metaTailMap[tailIndex] = slug;
-    takenTailIndices.push(tailIndex);
-  })
-
-  const shortTailLength = Math.max(metaTailMap.length, 100);
-  const shortTailOfPi = longTailOfPi.slice(0, shortTailLength);
-
-  return { metaTailMap, shortTailOfPi };
 }
