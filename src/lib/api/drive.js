0bee64c737fd686e7dfc6a4c6f6413c10456cd2b
Levi <levi@the-telling-detail.org>
2025-08-22 16:04:07 -0400

    refactor and modularize drive fetching logic

---
+++ src/lib/api/drive.js
@@ -0,0 +1,35 @@
+// @ts-nocheck
+import { parse } from 'csv-parse/browser/esm/sync';
+import slugify from 'slugify';
+
+export async function fetchCsvIndex() {
+  const sheetId = '1-BjOyI_0_cX46dbSZMrjVo0mF2sXDeFgmT8gScvOVLM';
+  const url = `https://docs.google.com/spreadsheets/d/${sheetId}/export?format=csv`;
+  
+  console.log('fetching index from Google Drive');
+  const response = await fetch(url);
+    
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  
+  const csvText = await response.text();
+  
+  return csvText;
+}
+
+export function parseCsvIndex(csv) {
+  const index = parse(csv, {
+    columns: true,
+    skip_empty_lines: true,
+    trim: true
+  });
+
+  const keyedIndex = {};
+  index.forEach(row => {
+    const slug = slugify(row.title, { lower: true, remove: /[*+~.,()'"!:@]/g });
+    keyedIndex[slug] = row;
+  });
+
+  return keyedIndex;
+}
--- src/lib/store.js
+++ src/lib/store.js
@@ -1,7 +1,6 @@
 // @ts-nocheck
 import { writable } from 'svelte/store';
-import { parse } from 'csv-parse/browser/esm/sync';
-import slugify from 'slugify';
+import { fetchCsvIndex, parseCsvIndex } from './api/drive';
 
 function createPoemIndexStore() {
   const { subscribe, set } = writable(null);
@@ -31,32 +30,11 @@ function createPoemIndexStore() {
 
           isLoading = true;
           
-          try {
-            const poemIndexSheetId = '1-BjOyI_0_cX46dbSZMrjVo0mF2sXDeFgmT8gScvOVLM';
-            const url = `https://docs.google.com/spreadsheets/d/${poemIndexSheetId}/export?format=csv`;
-            const response = await fetch(url);
-            const poemIndexCsv = await response.text();
-
-            const index = parse(poemIndexCsv, {
-              columns: true,
-              skip_empty_lines: true,
-              trim: true
-            });
-
-            const keyedIndex = {};
-            index.forEach(row => {
-              const slug = slugify(row.title, { lower: true, remove: /[*+~.,()'"!:@]/g });
-              keyedIndex[slug] = row;
-            });
-
-            set(keyedIndex);
-            resolve(keyedIndex);
-          } catch (error) {
-            console.error('Failed to fetch data:', error);
-            resolve(null);
-          } finally {
-            isLoading = false;
-          }
+          const indexCsv = await fetchCsvIndex();
+          const keyedIndex = parseCsvIndex(indexCsv);
+          set(keyedIndex);
+          resolve(keyedIndex);
+          isLoading = false;
         });
       });
     }
