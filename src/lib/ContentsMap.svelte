623d98254379d59b1f0ffdf7e479fa3aa7fb210f
Levi <levi@the-telling-detail.org>
2025-08-26 16:19:19 -0400

    add extent to map view

--- src/lib/ContentsMap.svelte
+++ src/lib/ContentsMap.svelte
@@ -21,6 +21,11 @@
 
   let map, tileLayer;
   const cityCentreCoords = [-79.3805366450235, 43.7302401508249];
+  const cityExtentCoords = [-79.6993, 43.56, -79.08, 43.8855];
+  const viewExtent = [
+    ...fromLonLat([cityExtentCoords[0], cityExtentCoords[1]]),
+    ...fromLonLat([cityExtentCoords[2], cityExtentCoords[3]])
+  ];
 
   function initializeMap(node) {    
     tileLayer = new VectorTileLayer({
@@ -62,7 +67,8 @@
         zoom: 12,
         minZoom: 12,
         maxZoom: 16,
-        rotation: 0.3
+        rotation: 0.3,
+        extent: viewExtent
       })
     });
 
