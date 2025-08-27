b0597a8b30d5b352856e78c6fa819cc0b372586d
Levi <levi@the-telling-detail.org>
2025-08-27 14:46:13 -0400

    rudimentary map marker click handler

--- src/lib/ContentsMap.svelte
+++ src/lib/ContentsMap.svelte
@@ -11,6 +11,9 @@
   import { defaults as defaultInteractions } from 'ol/interaction.js';
   import { Style, Circle, Fill, Stroke } from 'ol/style.js';
   import { poemIndex } from '$lib/store.js';
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
 
   const mapBoxApiKey = import.meta.env.VITE_MAPBOX_API_KEY;
   const mvtId = 'le0nl.streets-of-toronto';
@@ -19,6 +22,8 @@
   const vectorTileSource = new VectorTileSource({
     format: new MVT(), url: tileUrl 
   });
+
+  let markerId = $state('nobody'); 
 
   let map, tileLayer;
   const cityExtentCoords = [-79.6993, 43.56, -79.04, 43.87];
@@ -73,6 +78,17 @@
         rotation: 0.3,
         extent: viewExtent
       })
+    });
+
+     map.on('click', (event) => {
+      const feature = map.forEachFeatureAtPixel(event.pixel, (feature, layer) => {
+        if (layer === markerLayer) {
+          return feature;
+        }
+      });
+      if (feature) {
+        dispatch('markerClick', feature.get('id'));
+      }
     });
 
     return {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,29 @@
 <script>
   import ContentsMap from '$lib/ContentsMap.svelte';
+
+  let mIsForMap = $state(true);
+  let poemSlug = $state(null); 
+  
+  function mapMarkerClick(id) {
+    poemSlug = id;
+    mIsForMap = false;  
+  }
+
 </script>
 
-<ContentsMap />
+{#if mIsForMap}
+  <ContentsMap on:markerClick={e => mapMarkerClick(e.detail)} />
+{:else}
+  <div>
+    <h1>{ poemSlug }</h1>
+    <button onclick={ () => { mIsForMap = true} }>back</button>
+  </div>
+{/if}
+
+<style>
+  h1 {
+    color: yellow;
+    width: 100%;
+    text-align: center;
+  }
+</style>
