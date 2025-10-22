30c818cfaf60f9ee8b75a3a8dae41d33d997c186
Levi <levi@the-telling-detail.org>
2025-10-21 20:46:20 -0400

    piTailIndices are not set on the index google sheet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,19 @@
 <script>
-  import { poemIndex, piTail, poemTailIndexMap, extraTailEnd } from '$lib/store.js';
+  import { poemIndex, piTail, extraTailEnd, poemTailIndexMap } from '$lib/store.js';
   import IndexCard from '$lib/IndexCard.svelte';
 
   let { data } = $props();
 
-  const sefirahIds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const nCrossroads = 4713;
-  const shortTailLength = Math.min(Object.keys($poemIndex).length * 10, nCrossroads);
-
-  $piTail = data.longTailOfPi.slice(0, shortTailLength);
-  $extraTailEnd = data.longTailOfPi.slice(shortTailLength + 1, shortTailLength + 21);
-
-  const tailIndicesByInt = sefirahIds.map(int => {
-    const indices = $piTail.reduce((acc, val, idx) => {
-      if (val === int) acc.push(idx);
-      return acc;
-    }, []);
-    return indices;
-  });
-
+  const usedTailIndices = [];
   Object.keys($poemIndex).forEach(slug => {
-    const sefirahId = $poemIndex[slug].sefirahId;
-    const availableIncdices = tailIndicesByInt[sefirahId];
-    const [randomIndex] = availableIncdices.splice(Math.floor(Math.random() * availableIncdices.length), 1);
-    $poemTailIndexMap[randomIndex] = slug;
+    const index = Number($poemIndex[slug].piTailIndex);
+    usedTailIndices.push(index);
+    $poemTailIndexMap[index] = slug;
   });
+
+  const maxIndex = Math.max(...usedTailIndices);
+  $piTail = data.longTailOfPi.slice(0, maxIndex + 3);
+  $extraTailEnd = data.longTailOfPi.slice($piTail.length + 1, $piTail.length + 21);
 </script>
 
 <IndexCard />
