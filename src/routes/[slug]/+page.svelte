d06b708baeb31e4c9377c5b48503ce1bca854deb
Levi <levi@the-telling-detail.org>
2025-12-23 22:46:46 -0500

    add current poem to read store

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,12 +1,11 @@
 <script>
-  import { currentPoemId } from '$lib/store.js';
+  import { currentPoemId, poemsRead } from '$lib/store.js';
   import ThePage from '$lib/ThePage.svelte';
 
   let { data } = $props();
 
-  if ($currentPoemId !== data.slug ) {
-    $currentPoemId = data.slug;
-  }
+  if ($currentPoemId !== data.slug ) $currentPoemId = data.slug;
+  $poemsRead = [{ id: $currentPoemId, dateTime: new Date }, ...$poemsRead];
 </script>
 
 <ThePage />
